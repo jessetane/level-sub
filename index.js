7b0edd6f3c80e32d863b5f1450fcba0368a0ffff
Jesse Tane <jesse.tane@gmail.com>
2014-03-02 18:44:26 -0500

    DRY up prefix assignment to make tests pass

--- index.js
+++ index.js
@@ -2,49 +2,46 @@ var thru = require('thru');
 var sep = '\xff';
 
 module.exports = function(db) {
-  
+
   return sublevel();
-  
+
   function sublevel(parent, name) {
     name = name || '';
     parent = parent || '';
-    
+    var prefix = '';
+
     if (parent) {
       name = parent + sep + sep + name;
+      prefix = sep + name + sep;
     }
-    
-    var prefix = sep + name + sep;
-    
+
     return {
       sublevel: function(sub) {
         return sublevel(name, sub);
       },
 
       put: function(key, value, opts, cb) {
-        if (name) key = prefix + key;
         if (typeof opts === 'function') {
           cb = opts;
           opts = undefined;
         }
-        return db.put(key, value, opts, cb);
+        return db.put(prefix + key, value, opts, cb);
       },
 
       get: function(key, opts, cb) {
-        if (name) key = prefix + key;
         if (typeof opts === 'function') {
           cb = opts;
           opts = undefined;
         }
-        return db.get(key, opts, cb);
+        return db.get(prefix + key, opts, cb);
       },
 
       del: function(key, opts, cb) {
-        if (name) key = prefix + key;
         if (typeof opts === 'function') {
           cb = opts;
           opts = undefined;
         }
-        return db.del(key, opts, cb);
+        return db.del(prefix + key, opts, cb);
       },
 
       batch: function (ops_, cb) {
@@ -61,7 +58,7 @@ module.exports = function(db) {
 
       createWriteStream: function(opts) {
         var ws = db.createWriteStream(opts);
-        if (name) {
+        if (parent) {
           var w = ws;
           ws = thru(function(obj, cb) {
             obj.key = prefix + obj.key;
@@ -76,20 +73,20 @@ module.exports = function(db) {
       createReadStream: function(opts) {
         opts = opts || {};
         var end = '';
-      
-        if (name) {
+
+        if (parent) {
           if (opts.start) opts.start = prefix + opts.start;
           else opts.start = prefix;
           end = prefix;
         }
-      
+
         if (opts.end) opts.end = end + opts.end;
         else opts.end = end;
         opts.end += sep;
-      
+
         var rs = db.createReadStream(opts);
-      
-        if (name) {
+
+        if (parent) {
           var r = rs;
           rs = thru(function(obj, cb) {
             obj.key = obj.key.slice(prefix.length);
@@ -103,5 +100,4 @@ module.exports = function(db) {
       }
     };
   }
-  
 };
